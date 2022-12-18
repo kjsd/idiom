eaf9980040fd059c319fc03f0c43ec6b17fc6dd5
Kenji MINOURA <info@kandj.org>
2022-12-18 19:34:32 +0900

    desc

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function get_idioms() {
   const response = await fetch('https://dictionary.goo.ne.jp/idiom/');
   const body = await response.text();
   const root = parse(body);
-  const idioms =  root.querySelectorAll('.in-side-ttl-b').slice(0, 10).map(el => {
+  const idioms =  root.querySelectorAll('.in-side-ttl-b').map(el => {
     const result = /([^_（）]+)（(.+)）/.exec(el.text);
     return {
       kanji: result[1],
