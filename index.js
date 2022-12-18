13653a262b0d2ceb1f7cc2ae8f92b496d4c996cf
Kenji MINOURA <info@kandj.org>
2022-12-19 06:14:49 +0900

    num

--- index.js
+++ index.js
@@ -12,34 +12,52 @@ functions.http('entry', async (req, res) => {
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
   } else {
-    const data = await get_idioms();
+    const data = await get_idioms(req.query.n || 10);
     res.json(data);
   }
 });
 
-async function get_idioms() {
-  const response = await fetch('https://dictionary.goo.ne.jp/idiom/');
-  const body = await response.text();
-  const root = parse(body);
-  const idioms =  root.querySelectorAll('.in-side-ttl-b').map(el => {
-    const result = /([^_（）]+).*（(.+)）/.exec(el.text);
-    return {
-      kanji: result[1],
-      kana: result[2]
-    };
-  });
-  const descs = await Promise.all(idioms.map(v => get_description(v.kanji)));
-  return idioms.map((v,i) => Object.assign(v, { desc: descs[i]}));
+const firsts_ = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆ'
+      + 'よらりるれろわがぎぐげござじずぜぞだでどばびぶべぼ';
 
-}
-async function get_description(idiom) {
-  const uri = `https://dictionary.goo.ne.jp/word/${idiom}/`;
-  const response = await fetch(encodeURI(uri));
-  const body = await response.text();
-  const root = parse(body);
-  const el = root.querySelector('.meaning_area');
-  const desc = root.querySelector('.meaning_area').text;
-  const result =  /([^▽]+)▽.*/.exec(desc);
-  return result ? result[1]: desc;
+async function get_idioms(num) {
+  let n = 0;
+  const idioms = [];
+  while(n < num) {
+    const next = (num - n > 10) ? 10: num - n;
+    const i = random(0, firsts_.length);
+    const uri = `https://dictionary.goo.ne.jp/idiom/index/${firsts_.charAt(i)}`;
+    const response = await fetch(encodeURI(uri));
+    const body = await response.text();
+    const root = parse(body);
+    const els = random_sort(root.querySelectorAll('.title')).slice(0, next);
+    
+    els.forEach(el => {
+      const result = /^([^【]+)【(.+)】/.exec(el.text);
+      idioms.push({
+        kanji: result[2],
+        kana: result[1],
+        desc: el.nextElementSibling.text
+      });
+    });
+    
+    n += next;
+  }
+  return idioms;
 }
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function random_sort(org) {
+  const ary = org.concat();
+  let a = ary.length;
+  while (a) {
+    const j = Math.floor( Math.random() * a);
+    const t = ary[--a];
+    ary[a] = ary[j];
+    ary[j] = t;
+  }
+  return ary;
+}
