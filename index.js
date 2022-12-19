70abf4c57fcee10cd0a080dd911c317478aeadff
Kenji MINOURA <info@kandj.org>
2022-12-19 14:33:05 +0900

    easy

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ functions.http('entry', async (req, res) => {
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
   } else {
-    const data = await get_idioms(req.query.n || 10);
+    const data = await (req.query.easy ? get_popular_idioms(req.query.n || 10)
+                        : get_idioms(req.query.n || 10));
     res.json(data);
   }
 });
 
 const firsts_ = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆ'
       + 'よらりるれろわがぎぐげござじずぜぞだでどばびぶべぼ';
+
+async function get_popular_idioms(num) {
+  const response = await fetch('https://dictionary.goo.ne.jp/idiom/');
+  const body = await response.text();
+  const root = parse(body);
+  const els = root.querySelectorAll('.title,.in-side-ttl-b');
+  const idioms = random_sort(els).slice(0, num).map(el => {
+    const result = /^([^（]+)（(.+)）/.exec(el.text);
+    return {
+      kanji: result[1],
+      kana: result[2]
+    };
+  });
+  const descs = await Promise.all(idioms.map(v => get_description(v.kanji)));
+  return idioms.map((v,i) => Object.assign(v, { desc: descs[i]}));
+}
 
 async function get_idioms(num) {
   let n = 0;
@@ -45,6 +62,17 @@ async function get_idioms(num) {
     n += next;
   }
   return idioms;
+}
+
+async function get_description(idiom) {
+  const uri = `https://dictionary.goo.ne.jp/word/${idiom}/`;
+  const response = await fetch(encodeURI(uri));
+  const body = await response.text();
+  const root = parse(body);
+  const el = root.querySelector('.meaning_area');
+  const desc = root.querySelector('.meaning_area').text;
+  const result =  /([^▽]+)▽.*/.exec(desc);
+  return result ? result[1]: desc;
 }
 
 function random(min, max) {
